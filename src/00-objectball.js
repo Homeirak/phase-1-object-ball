c571acce0a6930ce22b5ef4d893f17836d8163fb
Yeji-Yoon <angelayoon0102@gmail.com>
2025-01-31 21:32:13 -0800

    complete lab

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -131,3 +131,134 @@ function homeTeamName(){
 //console.log(homeTeamName());
 console.log(gameObject()["home"]["players"]["Alan Anderson"]["number"]);
 console.log(gameObject()["home"]["colors"][0]);
+
+function homeObject(){
+    return gameObject()["home"]
+}
+function awayObject(){
+    return gameObject()["away"]
+}
+
+function mainOrnot(playername){
+    let newArray = gameObject()["home"]["players"]
+    if (newArray.hasOwnProperty(playername)){
+        return  homeObject()
+        
+    } else{
+        return  awayObject()
+    }
+
+    return
+}
+
+function numPointsScored(playername){
+        let shorterObject = mainOrnot(playername)
+        
+        return shorterObject["players"][playername]["points"]
+
+}
+
+console.log(numPointsScored("Jason Terry"));
+
+function shoeSize(playername){
+    let shorterObject = mainOrnot(playername)
+    return shorterObject["players"][playername]["shoe"]
+}
+
+console.log(shoeSize("Jason Terry"));
+
+function OthermainOrnot(teamname){
+    let newArray = gameObject()["home"]
+    if (newArray.hasOwnProperty(teamname)){
+        return  homeObject()
+        
+    } else{
+        return  awayObject()
+    }
+
+    return
+}
+
+function teamColors(teamname){
+    let shorterObject = OthermainOrnot(teamname)
+    return shorterObject["colors"]
+}
+
+console.log(teamColors("Jason Terry"));
+
+function teamNames(){
+    for (let key in homeObject())
+        if (key === "teamName"){
+            console.log(gameObject()["home"]["teamName"])
+        } 
+    
+    for (let key in awayObject())
+        if (key === "teamName"){
+                console.log(gameObject()["away"]["teamName"])
+         }  
+}
+
+console.log(teamNames())
+
+function playerNumbers(teamname){
+    let shorterObject = OthermainOrnot(teamname)
+     for (let key in shorterObject["players"]){
+        console.log(shorterObject["players"][key]["number"])
+     }
+}
+
+console.log(playerNumbers("Charlotte Hornets"))
+
+function playerStats(playername){
+    let shorterObject = mainOrnot(playername)
+    return shorterObject["players"][playername]
+
+}
+console.log(playerStats("Jason Terry"))
+
+function prebigShoeRebounds(){
+    let object = {}
+    //object has name:shoe 
+    let x = 0
+    let y =''
+    let a = 0
+    let b = ''
+    for (let key in gameObject()["home"]["players"]){
+        object.key =  gameObject()["home"]["players"][key]["shoe"]
+    }
+    for (let key in object){
+        if (object.key>x){
+            x=object.key
+            y=key
+        }
+    }
+    let lastobject={}
+    lastobject.y = x
+
+    for (let key in gameObject()["away"]["players"]){
+        object.key =  gameObject()["away"]["players"][key]["shoe"]
+    }
+    for (let key in object){
+        if (object.key>a){
+            a=object.key
+            b=key
+        }
+        return 
+    }
+    lastobject.b = a
+    //lastobject has name:shoe, name:shoe
+    if (lastobject.b>lastobject.y){
+        return b
+    } else{
+        return y
+    }             
+}
+
+function bigShoeRebounds(){
+    let playername = prebigShoeRebounds()
+    let shorterObject = mainOrnot(playername)
+    return shorterObject["players"][playername]["rebounds"]
+
+}
+
+console.log(a)
